26482dcd0860b7e93bb2a1ee862e18fb3066739e
Wydrian Matheus <wydrian.alexandre@gmail.com>
2020-05-04 02:55:06 -0300

    Projeto inicial do Slidy (Bloc+RxDart) com acesso ao FieBase

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:petvac/app/modules/home/home_module.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Slidy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StreamBuilder<Object>(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, snapshot) {
+
+          if (snapshot.connectionState == ConnectionState.active){
+            print("*** Estou conectado ao FireBase");
+          }
+
+          return HomeModule();
+        }
+      ),
+    );
+  }
+}
